0f57413999273c5a62387a27e52685feab735d04
José Ramón Balsas <jrbalsas@ujaen.es>
2023-04-02 13:32:44 +0200

    Jakarta 10 migration

--- src/main/java/com/daw/club/model/dao/ClienteDAOMap.java
+++ src/main/java/com/daw/club/model/dao/ClienteDAOMap.java
@@ -7,13 +7,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import javax.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped      //Elegible for Dependency Injection
 @DAOMap
 public class ClienteDAOMap implements ClienteDAO, Serializable{
 
-    private  Map<Integer, Cliente> clientes=null;
+    private  Map<Integer, Cliente> clientes;
     private  Integer idCliente = 1;
 
     public ClienteDAOMap() {
@@ -51,7 +51,7 @@ public class ClienteDAOMap implements ClienteDAO, Serializable{
         boolean result=false;
         if (clientes.containsKey(c.getId())) {
             Cliente nc=new Cliente(c);
-            clientes.replace(c.getId(),c);
+            clientes.replace(c.getId(),nc);
             result=true;
         }       
         return result;
